3830c48c2d7b2a19f4ececf2262f35500a494761
artemisiawinter <lit.son.of.hades@gmail.com>
2016-05-17 10:42:53 +0800

    deleted "notice" function - jquery

--- gamedev.js
+++ gamedev.js
@@ -1,11 +1,9 @@
 $( document ).ready(function() {
+
 $("#instructions").hide();
 
 $("#instructionbutton").click(function(){
 	$("#startpagetext").hide();
 	$("#instructions").show();
 }); //end of instructionbutton
-$("#exploreBody").click(function(){
-$("#notice").
-});//click exploreBody
 });//end of function
