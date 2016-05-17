d29613b8fda09f70f5c66378f50bed0276f76d66
artemisiawinter <lit.son.of.hades@gmail.com>
2016-05-17 11:00:39 +0800

    hide "x" and show "x"

--- gamedev.js
+++ gamedev.js
@@ -1,14 +1,17 @@
 $( document ).ready(function() {
 
 $("#instructions").hide();
+$("#x").hide();
 
 $("#instructionbutton").click(function(){
 	$("#startpgtext").hide();
 	$("#instructions").show();
+	$("#x").show();
 }); //end of instructionbutton
 
 $("#x").click(function(){
 	$("#startpgtext").show();
 	$("#instructions").hide();
+	$("#x").hide();
 })
 });//end of function
