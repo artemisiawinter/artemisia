3e5bc2f708ff5e41f1b1e22b9fcd7966aa724b51
rosegardensthorns <200859@hkis.edu.hk>
2016-05-19 10:20:14 +0800

    Key Cursor Att. 4

--- gamedev.js
+++ gamedev.js
@@ -4,6 +4,7 @@ $( document ).ready(function() {
 $("#instructions").hide();
 $("#x").hide();
 $("#prisondoor").hide();
+$("#goldenkey2").hide();
 
 $("#instructionbutton").click(function(){
 	$("#startpgtext").hide();
@@ -21,11 +22,20 @@ $("#goldenkey").click(function(){
 	$("#goldenkey").hide();
 	$("#exploreBody").css("background-image", "url(http://img.photobucket.com/albums/v90/subrosa_florens/oblivion/Oblivion348.jpg)");
 	$("#prisondoor").show();
-	$(document).on("mousemove", function(e){
-    $("#goldenkey2").css({
-       left:  e.pageX,
-       top:   e.pageY
-    });
+	$("#goldenkey2").show();
+	var $mouseX = 0, $mouseY = 0;
+var $xp = 0, $yp =0;
+
+$(document).mousemove(function(e){
+    $mouseX = e.pageX;
+    $mouseY = e.pageY;    
 });
+
+var $loop = setInterval(function(){
+// change 12 to alter damping higher is slower
+$xp += (($mouseX - $xp)/12);
+$yp += (($mouseY - $yp)/12);
+$("#goldenkey2").css({left:$xp +'px', top:$yp +'px'});  
+}, 30);
 })//goldenkey end
 });//end of function
