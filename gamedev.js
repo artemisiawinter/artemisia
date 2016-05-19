992b4269e12678d34732b7eb6fdd268e25031369
rosegardensthorns <200859@hkis.edu.hk>
2016-05-19 09:44:31 +0800

    Linking to Background 3 Att. 4

--- gamedev.js
+++ gamedev.js
@@ -17,7 +17,6 @@ $("#x").click(function(){
 
 $("#goldenkey").click(function(){
 	$("#goldenkey").hide();
-	$("#exploreBody").hide();
-	$("#background3").show();
+	$("#exploreBody").css("background-image", "url(http://vignette1.wikia.nocookie.net/elderscrolls/images/1/1e/%D0%9E%D0%B1%D1%83%D1%87%D0%B5%D0%BD%D0%B8%D0%B5.jpg/revision/latest?cb=20120729155649&path-prefix=ru)");
 })//goldenkey end
 });//end of function
