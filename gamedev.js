f5c922d6ab5c2e6c699ea8c25852337e0451dd69
rosegardensthorns <200859@hkis.edu.hk>
2016-05-23 10:27:51 +0800

    merge

---
+++ gamedev.js
@@ -0,0 +1,33 @@
+
+$( document ).ready(function() {
+
+$("#instructions").hide();
+$("#x").hide();
+$("#prisondoor").hide();
+$(".goldenkey2").hide();
+
+$("#instructionbutton").click(function(){
+	$("#startpgtext").hide();
+	$("#instructions").show();
+	$("#x").show();
+}); //end of instructionbutton
+
+$("#x").click(function(){
+	$("#startpgtext").show();
+	$("#instructions").hide();
+	$("#x").hide();
+})//end of "x"
+
+$("#goldenkey").click(function(){
+	$("#goldenkey").hide();
+	$("#exploreBody").css("background-image", "url(http://img.photobucket.com/albums/v90/subrosa_florens/oblivion/Oblivion348.jpg)");
+	$("#prisondoor").show();
+	$(".goldenkey2").show();
+$(document).mousemove(function(e) {
+    $('.goldenkey2').offset({
+        left: e.pageX,
+        top: e.pageY + 20
+    });
+});
+})//goldenkey end
+});//end of function
