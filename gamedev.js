a03fa8213c97847b6d41f4fb47b2ff49a1ae45de
rosegardensthorns <200859@hkis.edu.hk>
2016-05-25 11:14:17 +0800

    Wintersh

--- gamedev.js
+++ gamedev.js
@@ -7,6 +7,7 @@ $("#prisondoor").hide();
 $(".goldenkey2").hide();
 $("#metaldiv").hide();
 $("#deadend").hide();
+$("#wintersh").hide();
 
 $("#instructionbutton").click(function(){
 	$("#startpgtext").hide();
@@ -38,9 +39,14 @@ $("#metaldiv").click(function(){
 	$("#metaldiv").hide();
 	$("#exploreBody").css("background-image", "url(http://tespostcards.files.wordpress.com/2015/10/castle-dour-prision.jpg)");
 	$("#deadend").show();
+	$("#wintersh").show();
 });//end of metaldiv
 $("#deadend").click(function(){
 $("#exploreBody").css("background-image", "url(http://preview.turbosquid.com/Preview/2014/05/25__08_44_59/pic%202.jpgdcf7e882-62c2-4f49-b050-36d6bcba174fOriginal.jpg)");
 $("#deadend").hide();
 });// end of deadend
+$("#wintersh").click(function(){
+	$("#wintersh").hide();
+	$("exploreBody").css("background-image", "url(http://www.campture.com/blog/wp-content/uploads/2015/11/3763149320_0cb98c8b45_b.jpg)");
+});// end of wintersh
 });//end of function
