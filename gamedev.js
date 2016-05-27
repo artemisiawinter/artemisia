928dd154495d5590eef95aa46890f230f5548231
artemisiawinter <lit.son.of.hades@gmail.com>
2016-05-27 09:54:46 +0800

    added levanash to everything

--- gamedev.js
+++ gamedev.js
@@ -9,6 +9,7 @@ $("#metaldiv").hide();
 $("#deadend").hide();
 $("#wintersh").hide();
 $("#secondchancedoor").hide();
+$("#levanash").hide();
 
 $("#instructionbutton").click(function(){
 	$("#startpgtext").hide();
@@ -41,12 +42,14 @@ $("#metaldiv").click(function(){
 	$("#exploreBody").css("background-image", "url(http://tespostcards.files.wordpress.com/2015/10/castle-dour-prision.jpg)");
 	$("#deadend").show();
 	$("#wintersh").show();
+	$("#levanash").show();
 });//end of metaldiv
 
 $("#deadend").click(function(){
 	$("#exploreBody").css("background-image", "url(http://preview.turbosquid.com/Preview/2014/05/25__08_44_59/pic%202.jpgdcf7e882-62c2-4f49-b050-36d6bcba174fOriginal.jpg)");
 	$("#deadend").hide();
 	$("#wintersh").hide();
+	$("#levanash").hide();
 	$("#secondchancedoor").show();
 });// end of deadend
 
@@ -54,11 +57,21 @@ $("#wintersh").click(function(){
 	$("#deadend").hide();
 	$("#wintersh").hide();
 	$(".goldenkey2").hide();
+	$("#levanash").hide();
 	$("#exploreBody").css("background-image", "url(http://www.campture.com/blog/wp-content/uploads/2015/11/3763149320_0cb98c8b45_b.jpg)");
 });// end of wintersh
 
 $("#secondchancedoor").click(function(){
 	$("#exploreBody").css("background-image", "url(http://img.photobucket.com/albums/v90/subrosa_florens/oblivion/Oblivion348.jpg)");
 	$("#metaldiv").show();
-});
+}); //end of secondchancedoor
+
+$("#levanash").click(function(){
+	$("#exploreBody").css("background-image", "url(http://img.photobucket.com/albums/v90/subrosa_florens/oblivion/Oblivion348.jpg)");
+	$("#levanash").hide();
+	$("#deadend").hide();
+	$("#wintersh").hide();
+	$(".goldenkey2").hide();
+}); //end of levanash
+
 });//end of function
